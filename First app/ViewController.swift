197e708a68b4b872a2f45d1696fe9b51ed3b967f
Arel Guatno <arelguatno@yahoo.com>
2021-02-08 16:30:58 +0800

    first app

---
+++ First app/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  First app
+//
+//  Created by Arel Guatno on 2/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
+
